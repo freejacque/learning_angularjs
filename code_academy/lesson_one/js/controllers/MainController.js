dede0106f12df4a6410a69783e9962cc71576e31
freejacque <freejacque@gmail.com>
2016-09-12 20:26:22 -0400

    added name and price to controller

--- code_academy/lesson_one/js/controllers/MainController.js
+++ code_academy/lesson_one/js/controllers/MainController.js
@@ -1,4 +1,6 @@
 app.controller('MainController', ['$scope', function($scope) {
   $scope.title = 'Jacki\'s Angular App';
   $scope.promo = 'promo text';
+  $scope.name = 'The Books of Trees';
+  $scope.price = 19;
 }]);
