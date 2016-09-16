d92fa0f25d44a4c0a0dde35a54740030cf5f0857
freejacque <freejacque@gmail.com>
2016-09-16 08:55:44 -0400

    added more books to controller products

--- code_academy/lesson_one/js/controllers/MainController.js
+++ code_academy/lesson_one/js/controllers/MainController.js
@@ -13,6 +13,18 @@ app.controller('MainController', ['$scope', function($scope) {
                         price: 8,
                         pubdate: new Date('2013', '08', '01'),
                         cover: 'img/program-or-be-programmed.jpg'
+                      },
+                      {
+                        name: 'Pride and Prejudice',
+                        price: 7,
+                        pubdate: new Date('1813', '00', '28'),
+                        cover: 'http://theoverstuffedbookcase.com/wp-content/uploads/2015/09/0a7c2b7c19db0125c74164c6d0909d53.jpg'
+                      },
+                      {
+                        name: 'Boy, Snow, Bird',
+                        price: 10,
+                        pubdate: new Date('2014', '02', '16'),
+                        cover: 'http://melissastacy.com/wp-content/uploads/2014/08/Good-Cover.jpg
                       }
                     ]
 }]);
